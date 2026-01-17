1d1c278b678e559c8d6e909209e233d2e64ff6f7
Shimmer <36958351+sssheik1337@users.noreply.github.com>
2026-01-17 14:38:25 +0500

    Упростить навигацию дневника

--- static/components/navbar.js
+++ static/components/navbar.js
@@ -209,9 +209,7 @@ class CustomNavbar extends HTMLElement {
             </button>
             <div class="menu-panel" id="menu-panel">
               <a href="/profile" class="menu-link">Профиль</a>
-              <div class="menu-section">Дневник</div>
-              <a href="/diary?mode=summary" class="menu-link">Итоги дня</a>
-              <a href="/diary?mode=products" class="menu-link">По продуктам</a>
+              <a href="/diary" class="menu-link">Дневник питания</a>
               <a href="/foods" class="menu-link">Список продуктов</a>
               <a href="/menu#plans" class="menu-link">Тарифы</a>
               <span class="menu-link menu-link--disabled" title="Скоро">Настройки <span>Скоро</span></span>
