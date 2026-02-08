66df2099c76625fca5d9f1e33c2f1082becd0844
Shimmer <36958351+sssheik1337@users.noreply.github.com>
2026-02-08 14:54:05 +0500

    Вернуть запуск через main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from app.application import app
+import uvicorn
 
+from config import APP_HOST, APP_PORT
+from app.application import create_app
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    # Запуск приложения через единый входной файл.
+    uvicorn.run(app, host=APP_HOST, port=APP_PORT)
